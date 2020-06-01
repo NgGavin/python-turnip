5a3dce50563b8a12345527e8bd1ef92c116d36ce
Gavin Ng <gavinrng@gmail.com>
2020-06-01 11:16:33 -0400

    Extensive trends.py additions with decreasing. random, and inconclusive trend completion

--- trends.py
+++ trends.py
@@ -3,12 +3,15 @@ Created by: Gavin Ng
 Read README for more information about python-turnip in general and its associated files
 """
 
+# Global Setup
+global startincrease, startdecrease, increasing1, decreasing1, increasing2, decreasing2, increasing3, decreasing3
+startincrease, startdecrease, increasing1, decreasing1, increasing2, decreasing2, increasing3, decreasing3 = None, None, None, None, None, None, None, None
+
 
 # Determine the Trend Type
 def trendanalysis(buy_price, cyclepoints):
-	# Variable Setup
+	# Variable/List Setup
 	trendinterim = []
-	increasing1, decreasing1, increasing2, decreasing2, increasing3, decreasing3 = None, None, None, None, None, None
 	previousloop = "" # ('0' == increase, '1' == decrease)
 	cycleconverter = ["Monday AM","Monday PM","Tuesday AM","Tuesday PM","Wednesday AM","Wednesday PM","Thursday AM","Thursday PM","Friday AM","Friday PM","Saturday AM","Saturday PM"]
 
@@ -16,7 +19,7 @@ def trendanalysis(buy_price, cyclepoints):
 	for cycle in cyclepoints.values():
 		if cycle != None:
 			trendinterim.append(cycle)
-	
+
 	# Increasing and Decreasing Pattern Sorter
 	previous = 0
 	for current in len(trendinterim):
@@ -59,13 +62,13 @@ def trendanalysis(buy_price, cyclepoints):
 	# Random
 	if (3 >= decreasing1 >= 2) and startdecrease == True and cyclepoints[cycleconverter[0]] in range(buy_price * .6, buy_price * .8001):
 		randomtrend = True
-	elif (3 >= decreasing1 >= 2) and startdecrease == False and cyclepoints[cycleconverter[0]] in range(buy_price * .9, buy_price * 1.4001) and cyclepoints[cycleconverter[increasing1-1]] in range(buy_price * .6, buy_price * .8001):
+	elif (3 >= decreasing1 >= 2) and startincrease == True and cyclepoints[cycleconverter[0]] in range(buy_price * .9, buy_price * 1.4001) and cyclepoints[cycleconverter[increasing1-1]] in range(buy_price * .6, buy_price * .8001):
 		randomtrend = True
 	else:
 		randomtrend = False
 	
 	# Small Spike
-	if (5 >= increasing1 >= 1) and increasing2 == 0 and increasing3 == 0 and decreasing 3 == 0 and cyclepoints[cycleconverter[decreasing1 - 1]] in range(buy_price * .9, buy_price * 1.4001):
+	if (5 >= increasing1 >= 1) and increasing2 == 0 and increasing3 == 0 and decreasing3 == 0 and cyclepoints[cycleconverter[decreasing1 - 1]] in range(buy_price * .9, buy_price * 1.4001):
 		small_spiketrend = True
 	else:
 		small_spiketrend = False
@@ -92,27 +95,65 @@ def trendanalysis(buy_price, cyclepoints):
 def cyclesetup(buy_price, cyclepoints):
 	# Variable Setup
 	trendinterim = []
-	cycleoutput = ""
 	minimumcycle = []
 	maximumcycle = []
 	# Converts Dictionary Into A List For Analysis
 	for cycle in cyclepoints.values():
 		if cycle != None:
 			trendinterim.append(cycle)
+		elif len(trendinterim) < 12:
+			trendinterim.append(None)
 	for i in trendinterim:
 		minimumcycle.append(i)
 		maximumcycle.append(i)
-	return trendinterim, minimumcycle, maximumcycle, cycleoutput
+	return trendinterim, minimumcycle, maximumcycle
 
 # Decreasing Trend Type
 def decreasing(buy_price, cyclepoints):
-	trendinterim, minimumcycle, maximumcycle, cycleoutput= cyclesetup(buy_price, cyclepoints)
-	return cycleoutput
+	trendinterim, minimumcycle, maximumcycle = cyclesetup(buy_price, cyclepoints)
+	counter = 0
+	baserate = [trendinterim[0]/0.85,trendinterim[0]/0.9]
+	for i in trendinterim:
+		if i == None:
+			minimumcycle.append(trendinterim[counter]-(baserate[0]*0.05))
+			maximumcycle.append(trendinterim[counter]-(baserate[1]*0.03))
+		counter += 1
+	return minimumcycle,maximumcycle
 
 
 # Random Trend Type
 def random(buy_price, cyclepoints):
 	trendinterim, minimumcycle, maximumcycle, cycleoutput= cyclesetup(buy_price, cyclepoints)
+	counter = 0
+	decreasingcounter = None
+	baserate = [buy_price*0.6,buy_price*0.8]
+	for i in trendinterim:
+		if i == None:
+			if startincrease:
+				if i in range(increasing1 + decreasing1,increasing1 + decreasing1 + increasing2) or i in range(increasing1 + decreasing1 + increasing2 + decreasing2, increasing1 + decreasing1 + increasing2 + decreasing2 + increasing3):
+					minimumcycle.append(buy_price*0.9)
+					maximumcycle.append(buy_price*1.4)
+				elif i in range(increasing1 + decreasing1 + increasing2, decreasing2):
+					if decreasingcounter == True:
+						minimumcycle.append(baserate[0])
+						maximumcycle.append(baserate[1])
+					else:
+						minimumcycle.append(trendinterim[counter-1]-baserate[0]*.1)
+						maximumcycle.append(trendinterim[counter-1]-baserate[0]*0.04)
+					decreasingcounter = True
+			elif startdecrease:
+				if i in range(decreasing1,increasing1) or i in range(decreasing1 + increasing1 + decreasing2, increasing3):
+					minimumcycle.append(buy_price*0.9)
+					maximumcycle.append(buy_price*1.4)
+				elif i in range(decreasing1 + increasing1, decreasing2):
+					if decreasingcounter == True:
+						minimumcycle.append(baserate[0])
+						maximumcycle.append(baserate[1])
+					else:
+						minimumcycle.append(trendinterim[counter-1]-baserate[0]*.1)
+						maximumcycle.append(trendinterim[counter-1]-baserate[0]*0.04)
+					decreasingcounter = True
+		counter += 1
 	return cycleoutput
 
 
@@ -131,4 +172,8 @@ def large_spike(buy_price, cyclepoints):
 # Inconclusive Spike Trend Type
 def inconclusive(buy_price, cyclepoints):
 	trendinterim, minimumcycle, maximumcycle, cycleoutput= cyclesetup(buy_price, cyclepoints)
+	for i in trendinterim:
+		if i != None:
+			minimumcycle.append(9)
+			maximumcycle.append(660)
 	return cycleoutput
